6b20f5339d5e9d0b603fd0ef4f9331702f5a9719
Bartłomiej Sługocki <0@bartan0.pl>
2020-03-03 22:19:59 +0100

    lib: Added bindModel function / HOC

--- src/lib.js
+++ src/lib.js
@@ -2,5 +2,21 @@ const uuid = require('uuid/v4')
 
 module.exports = {
 	ID: () => '_' + uuid().replace(/-/g, ''),
-	PromiseResolve: (...args) => new Promise(r => r(...args))
+	PromiseResolve: (...args) => new Promise(r => r(...args)),
+
+	bindModel: Component => ({
+		model: renderable,
+		...props
+	}) => {
+		const [ model, updateModel ] = useState(() => renderable.getModel())
+
+		useEffect(() => {
+			renderable.on('update', () => updateModel(renderable.getModel()))
+		}, [])
+
+		return React.createElement(Component, {
+			model,
+			...props
+		})
+	}
 }
