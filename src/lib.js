216586ce94aff969d78847ba498a03931ffa622c
Bartłomiej Sługocki <0@bartan0.pl>
2020-03-14 22:12:25 +0100

    Lib: added function to render markdown

--- src/lib.js
+++ src/lib.js
@@ -1,15 +1,19 @@
-const React = require('react')
 const uuid = require('uuid/v4')
-
-const { useEffect, useState } = React
+const Markdown = require('markdown-it')
 
 module.exports = {
 	UUID: uuid,
+
 	PromiseResolve: (...args) => new Promise(r => r(...args)),
+
 	isDefined: x => x !== undefined,
+
 	bem: (block, elem, mod) => [
 		block,
 		elem && `__${elem}`,
 		mod && `--${mod}`
 	].join(''),
+
+	markdown: (markdown => text => markdown.render(text))
+		(new Markdown)
 }
