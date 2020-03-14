23d12978313e8f010a77dae3b53ebec452db2673
Bartłomiej Sługocki <0@bartan0.pl>
2020-03-14 20:28:00 +0100

    Added GContainer

---
+++ src/containers/gcontainer.js
@@ -0,0 +1,40 @@
+const GService = require('local/gservice')
+const { createElement, useEffect, useState } = require('react')
+const { Provider } = require('local/context')
+
+
+const GContainer = ({
+	autoConnect,
+
+	ready,
+	signedIn,
+	signedOut,
+}) => {
+	const [ isSignedIn, setSignedIn ] = useState(false)
+	const [ isInitialized, setInitialized ] = useState(false)
+	const render = isSignedIn
+		? isInitialized
+			? ready
+			: signedIn
+		: signedOut
+
+	useEffect(() => {
+		GService.on('signIn', () => setSignedIn(true))
+		GService.on('signOut', () => {
+			setSignedIn(false)
+			setInitialized(false)
+		})
+		GService.on('init', () => setInitialized(true))
+
+		if (autoConnect)
+			GService.connect()
+	}, [])
+
+	return render({
+		isSignedIn,
+		signIn: () => GService.signIn(),
+		signOut: () => GService.signOut()
+	})
+}
+
+module.exports = GContainer
