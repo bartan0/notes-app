bba38529f27acbe44e310132638132a2cd81177f
Bartłomiej Sługocki <0@bartan0.pl>
2020-03-15 23:41:15 +0100

    index.js: basic app

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
-require('regenerator-runtime')
 require('local/index.html')
+
+const React = require('react')
+const { render } = require('react-dom')
+const GContainer = require('local/gcontainer')
+const RootNode = require('local/node')
+const Consumer = require('local/consumers/root')
+
+
+const MainView = () =>
+	<RootNode consumer={Consumer}/>
+
+
+const App = () =>
+	<GContainer
+		autoConnect
+		signedIn={() => <div>wait...</div>}
+		signedOut={({ signIn }) =>
+			<div><button onClick={() => signIn()}>SIGN IN</button></div>
+		}
+		ready={MainView}
+	/>
+
+
+render(
+	React.createElement(App),
+	document.getElementById('app')
+)
