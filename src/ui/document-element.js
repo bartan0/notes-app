d922290184a8935c7fd28ea11e183bf4d3229c06
Bartłomiej Sługocki <0@bartan0.pl>
2020-04-01 00:07:59 +0200

    DocumentElement: Markdown support

--- src/ui/document-element.js
+++ src/ui/document-element.js
@@ -1,12 +1,49 @@
 const React = require('react')
 const { useGService } = require('local/containers/gservice')
+const { markdown } = require('local/lib')
+
+const { useState } = React
+
+// Values are meaningful
+const Mode = {
+	EDIT: 0,
+	SHOW: 1
+}
+
 
 const DocumentElement = ({ nodePath }) => {
-	const [ document ] = useGService(nodePath)
+	const [ document, documentStatus ] = useGService(nodePath)
+	const [ mode, setMode ] = useState(Mode.SHOW)
+
+	const update = content => {
+		setMode(1 - mode)
+		document.setContent(content)
+	}
+
 
 	return document ?
 		<div>
-			<textarea defaultValue={document.content} onBlur={({ target: t }) => document.setContent(t.value)}/>
+			<div>
+				<button onClick={() => setMode(1 - mode)}>
+					{mode === Mode.EDIT ?
+						'Show'
+					:
+						'Edit'
+					}
+				</button>
+			</div>
+
+			{mode === Mode.SHOW ?
+				<div
+					dangerouslySetInnerHTML={{ __html: markdown(document.content) }}
+					onClick={() => setMode(1 - mode)}
+				/>
+			:
+				<textarea
+					defaultValue={document.content}
+					onBlur={({ target: t }) => update(t.value)}
+				/>
+			}
 		</div>
 	:
 		null
