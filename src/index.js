da8de1cc14564a69235ae19661a4aca6bac453d3
Bartłomiej Sługocki <0@bartan0.pl>
2020-04-06 20:02:11 +0200

    Integrated Router into index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,72 @@
 require('local/main.sass')
 
 const React = require('react')
-const GServiceContainer = require('local/containers/gservice')
-const Router = require('local/router')
-const transformers = require('local/transformers')
 const { render } = require('react-dom')
-const { BrowserRouter } = require('react-router-dom')
+const { Status, withGService } = require('local/gservice')
+const transformers = require('local/transformers')
+const { BrowserRouter, Redirect, Route, Switch } = require('react-router-dom')
+const Dashboard = require('local/ui/dashboard')
+const Note = require('local/ui/note')
+const SignIn = require('local/ui/sign-in')
+const SignOut = require('local/ui/sign-out')
+
+const { createElement, useEffect } = React
 
 
-const App = () =>
-	<GServiceContainer
-		autoConnect
-		transformers={transformers}
-		render={gcontainer =>
-			<BrowserRouter>
-				<Router {...gcontainer}/>
-			</BrowserRouter>
-		}
-	/>
+const App = withGService(
+	transformers
+)(({
+	status,
+	connect,
+	signIn,
+	signOut
+}) => {
+	useEffect(() => {
+		if (status === Status.SIGNED_IN)
+			connect()
+	}, [
+		status
+	])
+
+	return (
+		<BrowserRouter>
+			{status !== Status.CONNECTED &&
+				<Switch>
+					<Route exact path="/sign-in" render={() =>
+						<SignIn onSignIn={signIn}/>
+					}/>
+					<Route exact path="/sign-out" render={() =>
+						<SignOut onSignOut={signOut}/>
+					}/>
+
+					<Redirect to="/sign-in"/>
+				</Switch>
+			}
+
+			{status === Status.CONNECTED &&
+				<Switch>
+					<Route exact path="/" render={() =>
+						<Dashboard/>
+					}/>
+					<Route exact path="/notes" render={() =>
+						<Dashboard onlyNotes/>
+					}/>
+					<Route exact path="/note/:id" render={({ match: { params } }) =>
+						<Note nodePath={`/${params.id}`}/>
+					}/>
+					<Route exact path="/sign-out" render={() =>
+						<SignOut/>
+					}/>
+
+					<Redirect to="/"/>
+				</Switch>
+			}
+		</BrowserRouter>
+	)
+})
+
 
 render(
-	React.createElement(App),
+	createElement(App),
 	document.getElementById('app')
 )
