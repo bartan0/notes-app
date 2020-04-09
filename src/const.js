831ffad8e7be5608da2fe8f6990693b17752a035
Bartłomiej Sługocki <0@bartan0.pl>
2020-04-09 16:16:05 +0200

    const.js: refactor

--- src/const.js
+++ src/const.js
@@ -1,19 +1,17 @@
-const constants = {
+module.exports = {
 	NodeType: {
 		NOTE: 'NOTE',
 		DOCUMENT: 'DOCUMENT',
 		CHECKLIST: 'CHECKLIST',
 		CHECKLIST_ITEM: 'CHECKLIST_ITEM'
-	}
+	},
+
+	NodeData: Object.fromEntries([
+		[ 'NOTE', [ 'New Note' ] ],
+		[ 'DOCUMENT', [ 'New Document' ] ],
+		[ 'CHECKLIST', [ 'New Checklist' ] ],
+		[ 'CHECKLIST_ITEM', [ 0, 'New Item' ] ]
+	]
+		.map(([ name, data ]) => [ constants.NodeType[name], data ])
+	)
 }
-
-constants.NodeData = Object.fromEntries([
-	[ 'NOTE', [ 'New Note' ] ],
-	[ 'DOCUMENT', [ 'New Document' ] ],
-	[ 'CHECKLIST', [ 'New Checklist' ] ],
-	[ 'CHECKLIST_ITEM', [ 0, 'New Item' ] ]
-]
-	.map(([ name, data ]) => [ constants.NodeType[name], data ])
-)
-
-module.exports = constants
