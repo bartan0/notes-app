0b8fc3da2e4b86e54cfbcd753607bf440862ae6e
Bartłomiej Sługocki <0@bartan0.pl>
2020-04-09 16:29:26 +0200

    Extracted Router from index

--- src/index.js
+++ src/index.js
@@ -2,80 +2,16 @@ require('local/global')
 require('local/main.sass')
 require('@fortawesome/fontawesome-free/css/all.css')
 
+const Router = require('local/router')
+const { BrowserRouter } = require('react-router-dom')
 const { render } = require('react-dom')
-const { Status, withGService } = require('local/gservice')
-const { BrowserRouter, Redirect, Route, Switch, useLocation } = require('react-router-dom')
-const Dashboard = require('local/ui/dashboard')
-const Note = require('local/ui/note')
-const SignIn = require('local/ui/sign-in')
-const SignOut = require('local/ui/sign-out')
 
-const { createElement, useEffect, useState } = React
-
-
-const App = withGService(({
-	status,
-	connect,
-	signIn,
-	signOut
-}) => {
-	const location = useLocation()
-	const [ targetLocation, setTargetLocation ] = useState(location)
-
-	useEffect(() => {
-		if (status === Status.SIGNED_IN)
-			connect()
-
-		if (status === Status.CONNECTED)
-			setTargetLocation(null)
-
-		/* DEV ONLY */
-		if (status === Status.INIT)
-			signIn()
-	}, [
-		status
-	])
-
-	return [
-		status !== Status.CONNECTED &&
-			<Switch key="not-connected">
-				<Route exact path="/sign-in" render={() =>
-					<SignIn onSignIn={signIn}/>
-				}/>
-				<Route exact path="/sign-out" render={() =>
-					<SignOut onSignOut={signOut}/>
-				}/>
-
-				<Redirect to="/sign-in"/>
-			</Switch>
-		,
-		status === Status.CONNECTED && targetLocation &&
-			<Redirect key="target-redirect" to={targetLocation}/>
-		,
-		!targetLocation &&
-			<Switch key="connected">
-				<Route exact path="/" render={() =>
-					<Dashboard/>
-				}/>
-				<Route exact path="/notes" render={() =>
-					<Dashboard onlyNotes/>
-				}/>
-				<Route exact path="/note/:id" render={({ match: { params } }) =>
-					<Note nodePath={`/${params.id}`}/>
-				}/>
-				<Route exact path="/sign-out" render={() =>
-					<SignOut/>
-				}/>
-
-				<Redirect to="/"/>
-			</Switch>
-	]
-})
+const { createElement } = React
 
 
 render(
 	createElement(BrowserRouter, null,
-		createElement(App)
+		createElement(Router)
 	),
 	document.getElementById('app')
 )
---
+++ src/router.js
@@ -0,0 +1,71 @@
+const { Status, withGService } = require('local/gservice')
+const { Redirect, Route, Switch, useLocation } = require('react-router-dom')
+
+const Dashboard = require('local/ui/dashboard')
+const Note = require('local/ui/note')
+const SignIn = require('local/ui/sign-in')
+const SignOut = require('local/ui/sign-out')
+
+const { useEffect, useState } = React
+
+
+const Router = withGService(({
+	status,
+	connect,
+	signIn,
+	signOut
+}) => {
+	const location = useLocation()
+	const [ targetLocation, setTargetLocation ] = useState(location)
+
+	useEffect(() => {
+		if (status === Status.SIGNED_IN)
+			connect()
+
+		if (status === Status.CONNECTED)
+			setTargetLocation(null)
+
+		/* DEV ONLY */
+		if (status === Status.INIT)
+			signIn()
+	}, [
+		status
+	])
+
+	return [
+		status !== Status.CONNECTED &&
+			<Switch key="not-connected">
+				<Route exact path="/sign-in" render={() =>
+					<SignIn onSignIn={signIn}/>
+				}/>
+				<Route exact path="/sign-out" render={() =>
+					<SignOut onSignOut={signOut}/>
+				}/>
+
+				<Redirect to="/sign-in"/>
+			</Switch>
+		,
+		status === Status.CONNECTED && targetLocation &&
+			<Redirect key="target-redirect" to={targetLocation}/>
+		,
+		!targetLocation &&
+			<Switch key="connected">
+				<Route exact path="/" render={() =>
+					<Dashboard/>
+				}/>
+				<Route exact path="/notes" render={() =>
+					<Dashboard onlyNotes/>
+				}/>
+				<Route exact path="/note/:id" render={({ match: { params } }) =>
+					<Note nodePath={`/${params.id}`}/>
+				}/>
+				<Route exact path="/sign-out" render={() =>
+					<SignOut/>
+				}/>
+
+				<Redirect to="/"/>
+			</Switch>
+	]
+})
+
+module.exports = Router
