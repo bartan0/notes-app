03aaf0bb9782f97238f6467bca6d3c3040c6e370
Bartłomiej Sługocki <0@bartan0.pl>
2020-04-23 22:15:52 +0200

    Restored working index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ require('local/global')
 require('local/main.sass')
 require('@fortawesome/fontawesome-free/css/all.css')
 
-const Router = require('local/router')
-const { BrowserRouter } = require('react-router-dom')
 const { render } = require('react-dom')
-
-const { createElement } = React
+const { BrowserRouter } = require('react-router-dom')
+const Router = require('local/router')
 
 
 render(
-	createElement(BrowserRouter, null,
-		createElement(Router)
-	),
-	document.getElementById('app')
+	<BrowserRouter>
+		<Router/>
+	</BrowserRouter>
+	,
+	document.body.appendChild(
+		document.createElement('div')
+	)
 )
